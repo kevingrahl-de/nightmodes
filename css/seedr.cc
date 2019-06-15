363122efd757776ef9dd21dd5054877457b7d933
Kevin Grahl <50027134+kevingrahl@users.noreply.github.com>
2019-06-15 02:27:07 +0200

    Create seedr.cc

---
+++ css/seedr.cc
@@ -0,0 +1,25 @@
+body, #files-tab, .content-item, .content-item-size, #files-heading, .content-heading, .header-row, .header-row .s-icon-column, footer {background:#000;color:#ffe;border-color:#222}
+.item-buttons i:hover{background:#444;color:#ffe;padding:3px 11px}
+.page-header{background:#222;color:#fff;border-color:#111}
+.medium-1.columns.table-heading{background-color:#000 !important;border-color:#222 !important}
+#files-heading .header-row > div:hover{background-color:#222;color:#fff}
+.account-upgrade{display:none}
+#upload-button, .right-menu-text-icon{background:#222;color:#fff;border-bottom-color:#444}
+#upload-button:hover, .right-menu-text-icon:hover{background-color:#444}
+header:hover #menu-drop{background-color:#444;color:#fff}
+#main-drop{background-color:#222}
+#subscription-menu-item{background:#444 !important;border-color:#444 !important}
+#main-drop li, div#wishlist-container h6, #add-folder-button{color:#ffe}
+#main-drop > ul > li:hover{background-color:#444}
+input[type="text"]{color:#fff}
+div#wishlist-container{background:#222;border-color:#222}
+div#wishlist-container ul{background:#222}
+div#wishlist-container li:hover{background:#444}
+.progress{background-color:#444;border-color:#444}
+#files-search-input{background-color:#222;border-color:#444}
+.fa-search:before{filter:invert(1)}
+#add-folder-button:hover{background:#222}
+.s-icon-column{color:#999}
+.support-button{background:#222;color:#ffe;border-color:#222}
+.progress.round{background:#222;border-color:#222}
+.progress-label{color:#fff}
